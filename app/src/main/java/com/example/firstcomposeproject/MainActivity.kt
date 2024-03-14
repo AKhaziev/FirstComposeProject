fc32f2ff1b54a64eb997a605b4d45e59998e7b7e
khaziev <alexander.khaziev@yandex.com>
2024-03-14 16:41:08 +0500

    Lesson 6

--- app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeproject/MainActivity.kt
@@ -3,95 +3,58 @@ package com.example.firstcomposeproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.firstcomposeproject.ui.theme.FirstComposeProjectTheme
+import com.example.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TimesTable()
-//            Greeting()
-//            UserInfo(name = "Александр", age = 43)
+            FirstComposeProjectTheme {
+                Column(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .fillMaxSize()
+                ) {
+
+                    InstagramProfileCard()
+                }
+
+            }
+
         }
     }
 }
 
 @Preview
 @Composable
-fun TimesTable() {
-
-    Column(
-        modifier = Modifier.fillMaxSize()
+fun CardTest() {
+    Card(
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        colors = CardDefaults.cardColors(),
+        border = BorderStroke(1.dp, Color.Black)
     ) {
-        for (i in 1 until 10) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) {
-                for (j in 1 until 10) {
-                    val color = if ((j + i) % 2 == 0) Color.Yellow else Color.White
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                            .border(width = 2.dp, color = Color.DarkGray)
-                            .background(color),
-                        contentAlignment = Alignment.Center,
-                    ) {
-                        Text(
-                            text = "${i * j}", color = Color.Black
-                        )
-                    }
-
-                }
-            }
-        }
-    }
-
-}
-
-//@Preview
-@Composable
-fun Greeting() {
-    val name = "Android"
-    Text(
-        text = "Hello $name!!!", color = Color.Green
-    )
-}
-
-//@Preview
-@Composable
-fun UserInfoPreview() {
-    UserInfo(name = "Alexander", age = 43)
-}
-
-
-@Composable
-fun UserInfo(name: String, age: Int) {
-
-    Column {
-        repeat(times = 10) {
-            Text(
-                text = "Hello $name, you are $age years old", color = Color.White
-            )
-        }
-
-
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = "Hello World!",
+            color = Color.Black
+        )
     }
 }
+
+
